e8f4d106bb28ca608d7b93d4142116c1d5c5a48c
Edoaru <pompom2006fumi@icloud.com>
2026-03-08 21:43:48 +0900

    add newMoneyItem Button

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Text, View } from "react-native";
+
 function MoneyItem({ label, amount }: { label: string; amount: number }) {
   return (
     <Text>
@@ -8,9 +10,9 @@ function MoneyItem({ label, amount }: { label: string; amount: number }) {
 }
 
 export default function Index() {
-  let budget = 50000;
-  let expense = 12000;
-  let balance = budget - expense;
+  const [budget, setBudget] = useState(50000);
+  const [expense, setExpense] = useState(12000);
+  const balance = budget - expense;
   return (
     <View
       style={{
@@ -23,6 +25,8 @@ export default function Index() {
       <MoneyItem label="今月の予算" amount={budget} />
       <MoneyItem label="現在の支出" amount={expense} />
       <MoneyItem label="今月の予算残高" amount={balance} />
+      <Button title="新しい予算" onPress={() => setBudget(30000)} />
+      <Button title="新しい支出" onPress={() => setExpense(5000)} />
     </View>
   );
 }
