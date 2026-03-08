ab026a8a68956a87c7516ebc00cf43c25474023a
Edoaru <pompom2006fumi@icloud.com>
2026-03-08 17:18:41 +0900

    Change app title's fontSize

--- app/index.tsx
+++ app/index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>家計簿アプリへようこそ！</Text>
+      <Text style={{ fontSize: 24 }}>家計簿アプリへようこそ！</Text>
       <Text>今月の予算：{budget.toLocaleString()}円</Text>
       <Text>現在の支出：{expense.toLocaleString()}円</Text>
       <Text>今月の予算残高：{balance.toLocaleString()}円</Text>
