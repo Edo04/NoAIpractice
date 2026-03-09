4b25e2036114d92afc392df48b1876a37a1c5879
Edoaru <pompom2006fumi@icloud.com>
2026-03-09 23:35:00 +0900

    make Input Buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 
 function MoneyItem({ label, amount }: { label: string; amount: number }) {
   return (
@@ -13,6 +13,8 @@ export default function Index() {
   const [budget, setBudget] = useState(50000);
   const [expense, setExpense] = useState(12000);
   const balance = budget - expense;
+  const [inputBudget, setInputBudget] = useState("");
+  const [inputExpense, setInputExpense] = useState("");
   return (
     <View
       style={{
@@ -25,8 +27,24 @@ export default function Index() {
       <MoneyItem label="今月の予算" amount={budget} />
       <MoneyItem label="現在の支出" amount={expense} />
       <MoneyItem label="今月の予算残高" amount={balance} />
-      <Button title="新しい予算" onPress={() => setBudget(30000)} />
-      <Button title="新しい支出" onPress={() => setExpense(5000)} />
+      <TextInput
+        style={{ borderWidth: 1, width: 200, padding: 8 }}
+        keyboardType="number-pad"
+        onChangeText={(text) => setInputBudget(text)}
+      />
+      <Button
+        title="新しい予算"
+        onPress={() => setBudget(Number(inputBudget))}
+      />
+      <TextInput
+        style={{ borderWidth: 1, width: 200, padding: 8 }}
+        keyboardType="number-pad"
+        onChangeText={(text) => setInputExpense(text)}
+      />
+      <Button
+        title="新しい支出"
+        onPress={() => setExpense(Number(inputExpense))}
+      />
     </View>
   );
 }
