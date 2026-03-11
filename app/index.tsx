833436b58b6805348b7efe15ff5517cf692b351c
Edoaru <pompom2006fumi@icloud.com>
2026-03-11 22:22:13 +0900

    separate index and list

--- app/index.tsx
+++ app/index.tsx
@@ -2,37 +2,12 @@ import { Link } from "expo-router";
 import { Text, View } from "react-native";
 import { useBudgetStore } from "../store/budgetStore";
 
-function CategoryItem({
-  category,
-  budget,
-  expense,
-}: {
-  category: string;
-  budget: number;
-  expense: number;
-}) {
-  return (
-    <View>
-      <Text>{category}</Text>
-      <Text>今月の予算：{budget.toLocaleString()}円</Text>
-      <Text>今月の支出：{expense.toLocaleString()}円</Text>
-    </View>
-  );
-}
-
 export default function Index() {
   const categories = useBudgetStore((state) => state.categories);
   const totalBudget = categories.reduce((acc, item) => acc + item.budget, 0);
   const totalExpense = categories.reduce((acc, item) => acc + item.expense, 0);
   const balance = totalBudget - totalExpense;
-  const categoryList = categories.map((item) => (
-    <CategoryItem
-      key={item.category}
-      category={item.category}
-      budget={item.budget}
-      expense={item.expense}
-    />
-  ));
+
   return (
     <View
       style={{
@@ -42,7 +17,8 @@ export default function Index() {
       }}
     >
       <Text style={{ fontSize: 24 }}>家計簿アプリへようこそ！</Text>
-      {categoryList}
+      <Text>今月の予算：{totalBudget.toLocaleString()}円</Text>
+      <Text>今月の支出：{totalExpense.toLocaleString()}円</Text>
       <Text>今月の残高：{balance.toLocaleString()}円</Text>
       <Link href="/list">一覧画面へ</Link>
     </View>
--- app/list.tsx
+++ app/list.tsx
@@ -1,9 +1,38 @@
 import { Text, View } from "react-native";
+import { useBudgetStore } from "../store/budgetStore";
+
+function CategoryItem({
+  category,
+  budget,
+  expense,
+}: {
+  category: string;
+  budget: number;
+  expense: number;
+}) {
+  return (
+    <View>
+      <Text>{category}</Text>
+      <Text>今月の予算：{budget.toLocaleString()}円</Text>
+      <Text>今月の支出：{expense.toLocaleString()}円</Text>
+    </View>
+  );
+}
 
 export default function List() {
+  const categories = useBudgetStore((state) => state.categories);
+  const categoryList = categories.map((item) => (
+    <CategoryItem
+      key={item.category}
+      category={item.category}
+      budget={item.budget}
+      expense={item.expense}
+    />
+  ));
   return (
     <View>
       <Text>一覧表示画面</Text>
+      {categoryList}
     </View>
   );
 }
